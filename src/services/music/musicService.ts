3f2914c3b41020a0b1c8d70c4b2f6074d6ce730e
Glen <glen.vandesteen@gmail.com>
2020-05-04 16:57:42 +0200

    dec

--- src/services/music/musicService.ts
+++ src/services/music/musicService.ts
@@ -21,7 +21,7 @@ export class MusicService {
         new Music({ title: "radio1", url: "http://icecast.vrtcdn.be/stubru-high.mp3", type: MusicTypes.Radio })
     ];
 
-    private musicDecibels = 0.05;
+    private musicDecibels = 0.001;
 
     constructor(@inject(TYPES.MessageResponder) private messageResponder: MessageResponder,
         @inject(TYPES.SongService) private songService: SongService,
